f9ec58bb9a8ecaa5310fc845fa50b442adccd361
Emanuel Oly <159573775+nicosnz@users.noreply.github.com>
2024-03-16 03:40:56 -0400

    vvvv

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -49,14 +49,14 @@ void exercise_3(int a, int b) {
 
 void exercise_4(double n, double a, double b, double x, double y) {
   // TODO: YOUR CODE HERE
-  double precio;
+  double precio,precio2;
   if(n>a){
     precio=n-(n*x)/(100.00);
     cout<<precio<<endl;
   }
-  else if((n>a) && (n>b)){
-    precio=n*0.875;
-    cout<<precio<<endl;
+  else if(n>a && n>b){
+    precio2=n*0.875;
+    cout<<precio2<<endl;
   }
   
   else{

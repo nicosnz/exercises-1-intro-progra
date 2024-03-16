1fa82fc80fd92bdca66f9c16d98fdbbc0be6b17d
Emanuel Oly <159573775+nicosnz@users.noreply.github.com>
2024-03-16 00:18:29 -0400

    1er commit

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -27,6 +27,15 @@ void exercise_5(char character) {
 
 void exercise_6(int number) {
   // TODO: YOUR CODE HERE
+   switch (number){
+    case 1:cout<<"Monday"<<endl;break;
+    case 2:cout<<"Tuesday"<<endl;break;
+    case 3:cout<<"Wednesday"<<endl;break;
+    case 4:cout<<"Thursday"<<endl;break;
+    case 5:cout<<"Friday"<<endl;break;
+    case 6:cout<<"Saturday"<<endl;break;
+    case 7:cout<<"Sunday"<<endl;break;
+    default:cout<<"Invalid input"<<endl;break;
 }
 
 void exercise_7(double r) {

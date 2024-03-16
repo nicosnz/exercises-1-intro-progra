4ccb6f9243dd75b08ec1ee3028e5b6490f7de9ae
Emanuel Oly <159573775+nicosnz@users.noreply.github.com>
2024-03-16 12:58:24 -0400

    primer intento ejercicio 15

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -151,7 +151,16 @@ string exercise_14(int number_of_docs) {
 
 void exercise_15(int a, int b, int c) {
   // TODO: YOUR CODE HERE
+  int aux;
+  cout<<"Los valores son; a = "<<a<< "b = "<<b<< "y c = "<<c<<endl;
   
+  //intercambios variables
+  aux=a;
+  a = b;
+  b = c;
+  c=aux;
+  cout<<"Permutamos: a => b, b => c, c => a"
+  cout<<"Los valores despues de la permutacion son; a = "<<a<< "b = "<<b<< "y c = "<<c<<endl;
 }
 
 void exercise_16(int debut, int fin) {
